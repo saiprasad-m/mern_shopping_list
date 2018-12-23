7fe44cf0e1e0060f0bb72a631af56ab44459cd95
Sai Prasad M <sai.prasad@rocketmail.com>
2018-12-24 00:39:12 +0530

    Adding client and backend in 1 app

---
+++ public/build/precache-manifest.d4123abd8c5bcbc1a1441cbc92963996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "533448f7443e80f1572d",
+    "url": "/static/js/main.533448f7.chunk.js"
+  },
+  {
+    "revision": "d80edf18ea8bf81d4e54",
+    "url": "/static/js/1.d80edf18.chunk.js"
+  },
+  {
+    "revision": "533448f7443e80f1572d",
+    "url": "/static/css/main.f049b32a.chunk.css"
+  },
+  {
+    "revision": "d80edf18ea8bf81d4e54",
+    "url": "/static/css/1.fb00c19c.chunk.css"
+  },
+  {
+    "revision": "e438cd86da09261bd53b7fd3a08fa1a9",
+    "url": "/index.html"
+  }
+];
