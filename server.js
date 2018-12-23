a12999c7f57633e867f088ab94a05f872fc4463a
Sai Prasad M <sai.prasad@rocketmail.com>
2018-12-24 00:41:28 +0530

    Adding CORS, client path in app

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+const path = require('path');
 
 const items = require("./routes/api/items");
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 
 const db = require("./config").mongoURI;
 
-mongoose
+  mongoose
   .connect(
     db,
     { useNewUrlParser: true }
   )
   .then(() => console.log("Mongodb connected"))
   .catch(err => console.log(err));
+
+  app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,'public','build','index.html'))
+})
 
 app.use("/api/items", items);
 
